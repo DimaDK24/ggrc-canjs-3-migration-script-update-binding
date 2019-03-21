61b8b9e1defc98540b4e85a833d86009ccd8b873
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-03-21 15:37:00 +0300

    refactor: remove useless for us functions
    Also, minorly improve code

--- src/can-migrate-script.js
+++ src/can-migrate-script.js
@@ -67,63 +67,12 @@ var transformJs = function (src, useImplicitBindings) {
   });
 };
 
-var transformHtml = function (src, useImplicitBindings) {
-  //find script tag with type text/stache
-  return src.replace(/(<script[^>]*type=("|')text\/stache\2[^>]*>)([\s\S]+?)(<\/script>)/g, function (fullStr, $1, quoteType, $3, $4) {
-    return $1 + transformStache($3, useImplicitBindings) + $4;
-  })
-    // find steal.js script tag
-    .replace(/(<script[^>]*src=("|').*steal\/steal\.js\2[^>]*>)([\s\S]+?)(<\/script>)/g, function (fullStr, $1, quoteType, $3, $4) {
-      return $1 + transformJs($3, useImplicitBindings) + $4;
-    });
-};
+module.exports = function transformer({source, path}, useImplicitBindings) {
+  const extension = path.slice(path.lastIndexOf('.') + 1);
 
-var transformMd = function (src, useImplicitBindings) {
-  //find ```html code blocks and treat it as stache
-  //find ```js code blocks and treat them as js
-  return src.replace(/(```)(js|javascript|html)?((?:[\s\S])+?)\1/g, function (fullStr, ticks, codeBlockType, codeBlock) {
-    codeBlockType = codeBlockType || '';
-    var output = ticks + codeBlockType;
-
-    if (codeBlockType === 'html') {
-      output += transformStache(codeBlock, useImplicitBindings);
-    } else if (codeBlockType === 'js' || codeBlockType === 'javascript') {
-      output += transformJs(codeBlock, useImplicitBindings);
-    } else {
-      output += codeBlock;
-    }
-    return output + ticks;
-  });
-};
-
-var transformComponent = function (src, useImplicitBindings) {
-  //find <template> or <view> tags and treat them as stache
-  return src.replace(/(<template>|<view>)([\s\S]+?)(<\/template>|<\/view>)/g, function (fullStr, $1, $2, $3) {
-    return $1 + transformStache($2, useImplicitBindings) + $3;
-  })
-    //find <view-model> or <script type="view-model"> tags and treat them as js
-    .replace(/(<view-model>|<script[^>]*type=("|')view-model\2[^>]*>)([\s\S]+?)(<\/view-model>|<\/script>)/g, function (fullStr, $1, quoteType, $3, $4) {
-      return $1 + transformJs($3, useImplicitBindings) + $4;
-    });
-};
-
-export default function transformer(file, api, options) {
-  var src = file.source;
-  var path = file.path;
-  var type = path.slice(path.lastIndexOf('.') + 1);
-  var useImplicitBindings = options.implicit;
-
-  if (type === 'js') {
-    src = transformJs(src, useImplicitBindings);
-  } else if (type === 'md') {
-    src = transformMd(src, useImplicitBindings);
-  } else if (type === 'html') {
-    src = transformHtml(src, useImplicitBindings);
-  } else if (type === 'stache') {
-    src = transformStache(src, useImplicitBindings);
-  } else if (type === 'component') {
-    src = transformComponent(src, useImplicitBindings);
+  if (extension === 'js') {
+    return transformJs(source, useImplicitBindings);
+  } else if (extension === 'stache') {
+    return transformStache(source, useImplicitBindings);
   }
-
-  return src;
 }
