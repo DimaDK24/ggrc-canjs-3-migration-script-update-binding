7069549708cb9f2991c6340afce18dfbc5aa9229
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-03-21 16:07:45 +0300

    test: add simple test to check can-migrate script

---
+++ __tests__/can-migrate-script.test.js
@@ -0,0 +1,19 @@
+const transformer = require('../src/can-migrate-script')
+
+const settings = {
+  path: 'stuff.stache'
+}
+const useImplicitBindings = false
+
+describe('transformer', () => {
+  it('should work with some very simple stache content', () => {
+    const source = `<advanced-search-filter-state
+            {model-name}="{modelName}">
+          </advanced-search-filter-state>`
+    const output = transformer({source, ...settings}, useImplicitBindings)
+    expect(output).toBe(`<advanced-search-filter-state
+            vm:modelName:from="{modelName}">
+          </advanced-search-filter-state>`)
+  })
+})
+
