1ac8a327f619554d16f0d542381e80c4305925ce
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-03-22 10:43:34 +0300

    feat: working transformer for paths

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const transformer = require('./can-migrate-script')
 const yargs = require('yargs').usage('Usage: $0 ...paths').argv
+const fs = require('fs')
 
 function main() {
   let paths = yargs._
-  console.log(paths);
+  paths.forEach((filePath) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) throw err
+      const transformedData = transformer({source: data, path: filePath}, false)
+      fs.writeFile(filePath, transformedData, (err) => {
+        if (err) throw err
+        console.log(`wrote ${filePath}`)
+      })
+    });
+  })
 }
 
 main()
